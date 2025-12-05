0ea408d4e027dec69486df55a42ec525dc7b7c15
Christopher Martinez <crmartinez239@protonmail.com>
2025-12-05 08:27:41 -0500

    Add comments

--- src/types/cell.js
+++ src/types/cell.js
@@ -1,7 +1,15 @@
+/*
+All values in ANS Forth are stored as unsigned 
+integers. Wether they are 32 bit or 64 bit depends 
+on the implementation. Forth.js uses 64 bit cells.
+*/
+
 "use strict";
 
 export class Cell {
     constructor(value) {
+        // JavaScript uses 53-bit precision for numbers,
+        // so we use BigInt to represent full 64-bit unsigned integers.
         this.value = BigInt.asUintN(64, BigInt(value));
     }
 
