5737ef050bed8048f7758825d022658caf4e250b
Christopher Martinez <crmartinez239@protonmail.com>
2025-12-05 07:43:33 -0500

    Remove unused math types

--- src/types/types.js
+++ src/types/types.js
@@ -1,14 +1,3 @@
-export const MathTypes = {
-    INVALID: 0,
-    VALUE: 1,
-    ADD: 2,
-    SUB: 3,
-    MUL: 4,
-    DIV: 5,
-    POWER: 6,
-    MODULUS: 7
-};
-
 export const StatusTypes = {
     OK: 'ok',
     COMPILED: 'compiled',
